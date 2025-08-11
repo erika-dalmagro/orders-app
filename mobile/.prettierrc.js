cb88c3e44f08c0e7b65d99007de4ae711fc7c91e
Érika Dal Magro <40403240+erika-dalmagro@users.noreply.github.com>
2025-08-10 21:29:04 -0300

    refactor (mobile): replace alerts with toasts

---
+++ mobile/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  bracketSpacing: false,
+  printWidth: 110,
+  arrowParens: "always",
+  bracketSpacing: true,
+};
